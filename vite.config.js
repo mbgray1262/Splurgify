85b38ad017cb950a0ef78e8b7966e46feb9c4676
Michael Gray <michaelgray@Michaels-MacBook-Air.local>
2025-05-25 18:09:45 +0100

    Fix structure

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  root: 'src', // Tells Vite to use src as the root (optional, see step 4)
+  build: {
+    outDir: '../dist',
+  },
+});
