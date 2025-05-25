d8f1e79cbf1bd0a2774526ded008c387f07a3a9f
Michael Gray <michaelgray@Michaels-MacBook-Air.local>
2025-05-25 20:44:31 +0100

    Fix vite.config.js to remove root and outDir

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // Tells Vite to use src as the root (optional, see step 4)
-  build: {
-    outDir: '../dist',
-  },
 });
